1a58b0ed7778606366252fe19d37dd421898c722
Maria Rosolovska <feofanmaria19@gmail.com>
2022-03-28 16:09:52 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$(function(){
+    $('.fairy-tail__slider, .our-trip__slider').slick({
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="./images/slider/prev_btn.png" alt=""></button>',
+
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="./images/slider/next_btn.png" alt=""></button>',
+
+        autoplay: true,
+        autoplaySpeed: 2500,
+        fade: true,
+
+        responsive: [
+            {
+                breakpoint: 601,
+                settings:{
+                    arrows: false
+                }
+            }
+        ]
+    });
+
+
+    $('.menu, .our-trip__body').on("click", "a", function (event){
+
+        event.preventDefault();
+
+        let id=$(this).attr('href'),
+
+        top=$(id).offset().top;
+
+        $(`body,html`).animate({ scrollTop: top}, 1500);
+    });
+});
+
+
